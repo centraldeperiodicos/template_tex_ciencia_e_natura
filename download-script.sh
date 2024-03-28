36de04f89762c37cfe6935ca531fb6bb11a43e7c
Pedro Lobo <lobophf@gmail.com>
2024-03-27 21:39:41 -0300

    Update download-script.sh

--- download-script.sh
+++ download-script.sh
@@ -4,14 +4,3 @@ source cen_scripts/fileDownloader.sh
 
 downloadFile "$author_url_files" "author_files"
 downloadFile "$ciencia_e_natura_url_files" "ciencia_e_natura_images"
-
-cleanup_files() {
-  rm -rf *.blg *.log *.spl *.aux *.out *.bbl
-}
-
-pdflatex paper.tex
-bibtex paper
-pdflatex paper.tex
-pdflatex paper.tex
-
-cleanup_files
